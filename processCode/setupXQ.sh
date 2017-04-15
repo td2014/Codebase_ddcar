fbf952063c8389010e4a4fe671e2db4a3d4e534f
td2014 <anthonyd2004@gmail.com>
2017-04-14 17:22:59 -0700

    Added some connections to host.

--- processCode/setupXQ.sh
+++ processCode/setupXQ.sh
@@ -4,10 +4,11 @@
 # local machine to XQuartz
 # From https://fredrikaverpil.github.io
 #
-###open -a XQuartz
+# Make sure to launch XQuartz first and call this script from an XQuartz window using the command: open -a XQuartz
+#
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $ip
-docker run -it -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/X11-unix ros
+docker run -it -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/X11-unix -v /Users/anthonydaniell/tmp_share:/tmp_host_share ros_vim_tut
 #
 # End of script
 #
