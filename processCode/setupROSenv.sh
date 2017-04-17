f66e5e57b3e254c7194296f1a37f8da5f1a49fc0
td2014 <anthonyd2004@gmail.com>
2017-04-17 06:57:14 -0700

    Added script to setup ROS directories.

---
+++ processCode/setupROSenv.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#
+# Script to configure ROS environment
+# (sourcing various setup.bash files)
+# Note:  This is to be run within (or through) the ROS docker image environments.
+#
+# By:  Anthony Daniell, 17 April 2017
+#
+. /opt/ros/kinetic/setup.bash
+. /tmp_host_share/workspace_dev/tutorial_ws/devel/setup.bash
+#
+# End of script
+#
